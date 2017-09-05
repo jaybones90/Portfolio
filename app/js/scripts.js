79c5a290f1e882c38b803f9d7f948fc202a4abbe
Jason Ainsworth <jasoncruze@gmail.com>
2017-09-04 19:56:19 -0700

    update styling for carosuel, create overlay that works on click

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,3 +3,10 @@ $('.project-container').slick({
   infinite: true,
   dots: true
 });
+
+$('.project').click(function(){
+  var $selected = $(this).attr('value');
+  var $overlay = $('.overlay');
+  $('.overlay').show();
+  $('.overlay').text('hey')
+})
