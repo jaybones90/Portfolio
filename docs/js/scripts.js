e52322c6cc8008528ddc2cb852813369fc82bf29
Jason Ainsworth <jasoncruze@gmail.com>
2017-09-14 13:49:38 -0700

    add in tech details for project, add close functionality after opening project preview, add in gif carousel, change styling

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,7 +3,9 @@ $('.project-container').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   infinite: true,
-  dots: true
+  dots: true,
+  prevArrow: "<img class='slick-prev' src='img/left-arrow-icon.png'>",
+  nextArrow: "<img class='slick-next' src='img/right-arrow-icon.png'>"
 });
 
 
@@ -32,15 +34,27 @@ $('.headline').fadeIn(2000);
 
 
 // click function for project grid to display preview and start slick
-$('.project-tile').click(function(e){
-  $('.project-tile').removeClass('selectedItem')
-  // $selectedItem.removeClass('.selectedItem');
-  let $target = $(e.currentTarget);
-  $target.addClass('selectedItem');
-  $('.project-description').hide();
-  let $targetId = $target.attr('data-target');
-  $(`#${$targetId}-description`).removeClass('hidden').hide().fadeIn(700);
-  // $('.project-row2').css('margin-top', '-4em');
-  let $slick = $(`#${$targetId}-slick`);
-  $slick.slick()
-})
+function projectDisplay() {
+var lastClick;
+  $('.project-tile').click(function(e){
+    let $target = $(e.currentTarget);
+    let $targetId = $target.attr('data-target');
+    let $slick = $(`#${$targetId}-slick`);
+    if (lastClick === $targetId && ($('.project-tile').hasClass('selectedItem'))) {
+      $('.project-description').hide();
+      $('.project-tile').removeClass('selectedItem');
+    } else {
+      lastClick = $targetId;
+      $('.project-tile').removeClass('selectedItem');
+      $target.addClass('selectedItem');
+      $('.project-description').hide();
+      $(`#${$targetId}-description`).removeClass('hidden').fadeToggle(1000);
+      $slick.slick({
+        prevArrow: "<img class='slick-prev' src='img/left-arrow-icon.png'>",
+        nextArrow: "<img class='slick-next' src='img/right-arrow-icon.png'>"
+      });
+    };
+  });
+};
+
+projectDisplay();
