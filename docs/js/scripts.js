aa646982e17ccffa20d3fef27006364a0ecc895d
Jason Ainsworth <jasoncruze@gmail.com>
2017-09-18 15:47:10 -0700

    add skydiving image into headline section, rearrange projects, add click function to change gif based on cliked project, add in gifs for catspot

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -8,8 +8,6 @@ $('.project-container').slick({
   nextArrow: "<img class='slick-next' src='img/right-arrow-icon.png'>"
 });
 
-
-
 // hover for project orbit
 $('.project').hover(
   function jerry()  {
@@ -20,6 +18,7 @@ $('.project').hover(
     $selected.fadeOut(600);
   }
 );
+
 // smooth scroll function
 $('a[href*="#"]').click(function(){
   let hash = this.hash;
@@ -29,17 +28,23 @@ $('a[href*="#"]').click(function(){
     window.location.hash = hash;
   });
 });
+
 // fade in headline
 $('.headline').fadeIn(2000);
 
-
 // click function for project grid to display preview and start slick
 function projectDisplay() {
+// set variable outside of click function to hold previously clicked project
 var lastClick;
   $('.project-tile').click(function(e){
+// set variables
     let $target = $(e.currentTarget);
     let $targetId = $target.attr('data-target');
     let $slick = $(`#${$targetId}-slick`);
+    let $gifSlide = $target.attr('data-value') || 0;
+// change slide in gif orbit to project that was clicked on
+    $('.project-container').slick('slickGoTo', $gifSlide )
+// closes project details if already open, else opens project details
     if (lastClick === $targetId && ($('.project-tile').hasClass('selectedItem'))) {
       $('.project-description').hide();
       $('.project-tile').removeClass('selectedItem');
@@ -49,6 +54,7 @@ var lastClick;
       $target.addClass('selectedItem');
       $('.project-description').hide();
       $(`#${$targetId}-description`).removeClass('hidden').fadeToggle(1000);
+// initializes slick carousel for screenshots in project details
       $slick.slick({
         prevArrow: "<img class='slick-prev' src='img/left-arrow-icon.png'>",
         nextArrow: "<img class='slick-next' src='img/right-arrow-icon.png'>"
